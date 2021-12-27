81280ece3a9a5775b654a5183e2cc9f6bb605e04
Weverton Ferreira <contato.wevertonfelipe@gmail.com>
2021-12-27 19:54:25 -0300

    First Commit

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DevFreela.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly OpeningTimeOption _options;
+        public ProjectsController(IOptions<OpeningTimeOption> options)
+        {
+            _options = options.Value;
+        }
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id) 
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProject) 
+        {
+            return CreatedAtAction(nameof(GetById), new { id = createProject.Id }, createProject);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject) 
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) 
+        {
+            return NoContent();
+        }
+
+        [HttpPost("{id}/comments")]
+        public IActionResult PostComment(int id, [FromBody] CreateCommentModel createComent)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id}/start")]
+        public IActionResult Start()
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id}/finish")]
+        public IActionResult Finish(int id)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ DevFreela.Core/Entities/Skills.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DevFreela.Core.Entities
+{
+    public class Skills
+    {
+        public Skills(string description, DateTime createdAt)
+        {
+            Description = description;
+            CreatedAt = DateTime.Now;
+        }
+        public string Description { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+    }
+}
